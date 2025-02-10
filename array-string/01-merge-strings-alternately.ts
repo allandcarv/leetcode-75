9dded80e80329e4b2f103395478515a96b9c4fac
Allan Carvalho <allan.carvalho@dynatrace.com>
2025-02-10 09:17:33 +0100

    feat: merge strings alternately

--- array-string/01-merge-strings-alternately.ts
+++ array-string/01-merge-strings-alternately.ts
@@ -1,8 +1,9 @@
 /**
-You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, 
-starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
 
 Return the merged string.
+
+ 
 
 Example 1:
 Input: word1 = "abc", word2 = "pqr"
@@ -11,16 +12,16 @@ Explanation: The merged string will be merged as so:
 word1:  a   b   c
 word2:    p   q   r
 merged: a p b q c r
-
 Example 2:
+
 Input: word1 = "ab", word2 = "pqrs"
 Output: "apbqrs"
 Explanation: Notice that as word2 is longer, "rs" is appended to the end.
 word1:  a   b 
 word2:    p   q   r   s
 merged: a p b q   r   s
-
 Example 3:
+
 Input: word1 = "abcd", word2 = "pq"
 Output: "apbqcd"
 Explanation: Notice that as word1 is longer, "cd" is appended to the end.
@@ -35,11 +36,11 @@ Constraints:
 word1 and word2 consist of lowercase English letters.
  */
 
-const mergeAlternately = (word1: string, word2: string): string => {
+function mergeAlternately(word1: string, word2: string): string {
+  const longer = Math.max(word1.length, word2.length);
   let result = '';
-  const longestWord = Math.max(word1.length, word2.length);
 
-  for (let i = 0; i < longestWord; i++) {
+  for (let i = 0; i < longer; i++) {
     if (word1[i]) {
       result += word1[i];
     }
@@ -50,8 +51,4 @@ const mergeAlternately = (word1: string, word2: string): string => {
   }
 
   return result;
-};
-
-console.log(mergeAlternately('abc', 'pqr'));
-console.log(mergeAlternately('ab', 'pqrs'));
-console.log(mergeAlternately('abcd', 'pq'));
+}
