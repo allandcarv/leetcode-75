8a97ef02ac3927bed31de9acf1f984fb835785e3
Allan Carvalho <allan.carvalho@dynatrace.com>
2025-02-10 09:53:14 +0100

    feat: kids with the greatest number of candies

--- array-string/03-kids-with-the-greatest-number-of-candies.ts
+++ array-string/03-kids-with-the-greatest-number-of-candies.ts
@@ -1,11 +1,9 @@
 /**
- There are n kids with candies. You are given an integer array candies, where each 
- candies[i] represents the number of candies the ith kid has, and an integer 
- extraCandies, denoting the number of extra candies that you have.
+There are n kids with candies. You are given an integer array candies, where each candies[i] represents 
+the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
 
-Return a boolean array result of length n, where result[i] is true if, after 
-giving the ith kid all the extraCandies, they will have the greatest number 
-of candies among all the kids, or false otherwise.
+Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the 
+extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
 
 Note that multiple kids can have the greatest number of candies.
 
@@ -28,7 +26,7 @@ Kid 1 will always have the greatest number of candies, even if a different kid i
 Example 3:
 Input: candies = [12,1,12], extraCandies = 10
 Output: [true,false,true]
- 
+
 Constraints:
 n == candies.length
 2 <= n <= 100
@@ -37,7 +35,7 @@ n == candies.length
  */
 
 function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
-  const maxNumberOfCandies = Math.max(...candies);
+  const greatest = Math.max(...candies);
 
-  return candies.map((kid) => kid + extraCandies >= maxNumberOfCandies);
+  return candies.map((kid) => kid + extraCandies >= greatest);
 }
