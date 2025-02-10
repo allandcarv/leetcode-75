fa138ebeca7a5efa62f5c2af03ea90292e7d92b9
Allan Carvalho <allan.carvalho@dynatrace.com>
2025-02-10 16:22:48 +0100

    feat: move zeroes

--- two-pointers/10-move-zeroes.ts
+++ two-pointers/10-move-zeroes.ts
@@ -1,6 +1,6 @@
 /**
-Given an integer array nums, move all 0's to the end of it while maintaining the 
-relative order of the non-zero elements.
+Given an integer array nums, move all 0's to the end of it while 
+maintaining the relative order of the non-zero elements.
 
 Note that you must do this in-place without making a copy of the array.
 
@@ -19,20 +19,17 @@ Constraints:
 Follow up: Could you minimize the total number of operations done?
  */
 
-/**
- Do not return anything, modify nums in-place instead.
- */
 const moveZeroes = (nums: number[]): void => {
-  let nonZeroIdx = 0;
+  let ptr = 0;
 
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] !== 0) {
-      nums[nonZeroIdx] = nums[i];
-      nonZeroIdx += 1;
+      nums[ptr] = nums[i];
+      ptr += 1;
     }
   }
 
-  nums.fill(0, nonZeroIdx);
+  nums.fill(0, ptr);
 };
 
 const nums1 = [0, 1, 0, 3, 12];
